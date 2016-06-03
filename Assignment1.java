d6413d999e366f506ddbe2cb6c792c44a9d48b48
Pooja-Karpey <poojakarpey92@gmail.com>
2016-06-03 02:26:30 +0000

    Add files via upload

---
+++ Assignment1.java
@@ -0,0 +1,15 @@
+
+public class Assignment1 {
+
+	public static void main(String args[]){
+		
+		int num=1;
+		float num2=  3.88f;
+		int sum;
+		sum=  num+(int) num2;
+		
+	System.out.print(sum);
+		
+	}
+	
+}
